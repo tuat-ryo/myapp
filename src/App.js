a8ff076dd5d330a7c4ab208692930656912b2e4a
chiyo <chiyo@TRT-Pro.local>
2018-06-03 11:20:52 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 import Toppage from './toppage.jsx';
-import Works from './works.jsx'
-import Op from './op.jsx'
-import Posts from './posts.jsx'
-import Post from './post.jsx'
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Link,
-  browserHistory,
-  withRouter,
-} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-        <Router>
-          <div style={{position:'absolute',width: '100vw',height:'100vh'}}>
-            <Route exact path='/' component={Toppage} />
-            <Route exact path='/works' component={Works} />
-            <Route exact path='/posts' render={props => <Posts fil='/blog.json' lin = '/posts/'  {...props} />} />
-            <Route path='/posts/:id' render={props => <Post text='/post-sources/' {...props} />} />
-          </div>
-       </Router>
+      <div className="App">
+      <Toppage/>
+      </div>
     );
   }
 }
